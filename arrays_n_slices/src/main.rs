4acacc6a1f0f0146f7b251f7e62057a8af6651b1
michaelkunc <mjkunc@gmail.com>
2018-02-14 08:26:56 -0800

    arrays and slices

--- arrays_n_slices/src/main.rs
+++ arrays_n_slices/src/main.rs
@@ -1,4 +1,4 @@
-use std::mem
+use std::mem;
 
 fn analyize_slice(slice: &[i32]){
     println!("The first element of the slices is {}", slice[0]);
@@ -9,4 +9,18 @@ fn main() {
     let xs: [i32; 5] = [1,2,3,4,5];
     let ys: [i32; 500] = [0; 500];
 
+    println!("This first element is {}", xs[0]);
+    println!("The second element is {}", xs[1]);
+
+    println!("array size {}", xs.len());
+
+    println!("Array occupies {} bytes", mem::size_of_val(&xs));
+
+    println!("Borrow the whole array as a slice \n");
+    analyize_slice(&xs);
+
+    println!("Borrow a section as a slice \n");
+    analyize_slice(&ys[1..4]);
+
+    println!("{}", xs[5]);
 }
---
+++ arrays_n_slices/target/debug/arrays_n_slices.d
@@ -0,0 +1,1 @@
+/Users/michaelkunc/Development/rust_by_example/arrays_n_slices/target/debug/arrays_n_slices.dSYM: /Users/michaelkunc/Development/rust_by_example/arrays_n_slices/src/main.rs
